9517326e333435979b5124e90eb14c4777f851a2
Aubrey Belford <aubreybelford1@gmail.com>
2026-02-11 21:36:39 -0500

    Proj01 Step 10 modified files for step 10

---
+++ file2.py
@@ -0,0 +1,7 @@
+MESSAGE = "Hello from file 2"
+print(MESSAGE)
+
+x = 21
+y = 3
+
+print(x + y)
---
+++ file3.py
@@ -0,0 +1,6 @@
+MESSAGE = "Hello from file 3"
+print(MESSAGE)
+
+print("Adding something to file3")
+
+print("Testing")
